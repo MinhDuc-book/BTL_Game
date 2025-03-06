c965c889aa0ef10f3c75f97485ea07c22ec22919
Minh Đức <mduc7325@gmail.com>
2025-03-06 15:42:17 +0700

    Add character and move's character

--- main.cpp
+++ main.cpp
@@ -1,53 +1,6 @@
 #include "header/includeFile.h"
 
-using namespace std;
-
-// kiểm tra khởi tạo
-bool init()
-{
-    bool success = true;
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        cout << "SDL could not initalized %s\n" << SDL_GetError() << endl;
-        success = false;
-    } else if (TTF_Init() < 0) {
-        cout << "Cannot initialize TTF" << TTF_GetError() << endl;
-        success = false;
-    } else {
-        gWindow = SDL_CreateWindow("AimGame", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED, SCREEN_W,SCREEN_H,SDL_WINDOW_SHOWN);
-        if (gWindow == NULL) {
-            cout << "Window could not initalized %s\n" << SDL_GetError() << endl;
-            success = false;
-        } else {
-            // tạo renderer cho cửa sổ
-            gRenderer = SDL_CreateRenderer(gWindow, - 1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-            if (gRenderer == NULL) {
-                cout << "Renderer could not created! SDL_ERROR:" << SDL_GetError() << endl;
-                success = false;
-            } else {
-                // khởi tạo con trỏ chuột mới
-                SDL_Surface *attackPointer = IMG_Load ("Icon/Ping_Target.png");
-                SDL_Surface *defaultPointer = IMG_Load ("Icon/defaultCursor.png");
-                if (attackPointer == NULL) {
-                    cout << "Failed to load image" << IMG_GetError() << endl;
-                    success = false;
-                } else {
-                    attackCursor = SDL_CreateColorCursor(attackPointer, attackPointer->w / 2, attackPointer->h /2);
-                    defaultCursor = SDL_CreateColorCursor(defaultPointer, 0, 0);
-                    if (attackCursor == nullptr) {
-                        cout << "Failed to load cursor" << SDL_GetError() << endl;
-                        success = false;
-                    }
-                    SDL_FreeSurface(attackPointer);
-                    SDL_FreeSurface(defaultPointer);
-                    attackPointer = nullptr;
-                    defaultPointer = nullptr;
-                };
-            }
-        }
-    }
-
-    return success;
-}  
+using namespace std; 
 
 // định dạng cho hình vuông
 void renderSquare(int x, int y, int size)
@@ -75,7 +28,19 @@ bool isMouseInSquare(int x_mouse, int y_mouse, int x_square, int y_square, int s
     return false;
 }
 
+bool isInRange (float range,Soldier soldier, int x_square, int y_square) {
+    float dx = soldier.X - x_square;
+    float dy = soldier.Y - y_square;
+    float dis = sqrt(dx*dx + dy*dy);
+
+    if (dis <= range) {
+        return true;
+    }
+    return false;
+}
+
 SDL_Event e;
+
 // xử lí phím nhấn (hàm xử lí sự kiện)
 KeyPress handleInput() {
     while (SDL_PollEvent(&e) != 0) {
@@ -152,7 +117,8 @@ int main (int argv, char *argc[]) {
                     break;
                 
             }
-
+            Soldier soldier = {1000, 1, SCREEN_W/2, SCREEN_H/2, 0, 0, 20};
+            int dRange = 0;
             SDL_SetCursor(defaultCursor);
             while (gameStart){
                 KeyPress pressInGame;
@@ -160,16 +126,19 @@ int main (int argv, char *argc[]) {
                 
                 while (SDL_PollEvent(&e) != 0) {
                     if (e.type == SDL_MOUSEBUTTONDOWN) {
-                        if (e.button.button == SDL_BUTTON_RIGHT) {
+                        if (e.button.button == SDL_BUTTON_LEFT) {
                             SDL_SetCursor(defaultCursor);
-                            // lấy vị trí của con chuột vào x_mouse, y_mouse
-                            // nếu click trong hình vuông thì thực hiện xóa cái cũ random cái mới
+                            dRange = 0;
                             if (isMouseInSquare(x_mouse, y_mouse, x_pos, y_pos, square_size)) {
                                 x_pos = rand() % (SCREEN_W - square_size);
                                 y_pos = rand () % (SCREEN_H - square_size);
                             }
-                        } if (e.button.button == SDL_BUTTON_LEFT) {
-                            // HIỆN THÔNG SỐ CỦA MỤC TIÊU (COMMING SOON)
+                        } if (e.button.button == SDL_BUTTON_RIGHT) {
+                            
+                            x_end = e.button.x;
+                            y_end = e.button.y;
+                            v = 5;
+                            
                         }
                     }
                     if (e.type == SDL_KEYDOWN) {
@@ -178,15 +147,20 @@ int main (int argv, char *argc[]) {
                             gameStart = false;
                         }
                         if (e.key.keysym.sym == SDLK_a || e.key.keysym.sym == SDLK_TAB) {
-                            SDL_ShowCursor(SDL_ENABLE);
+                            dRange++;
+                            dRange %= 2;
                             SDL_SetCursor(attackCursor);
                         }
                     } 
-                    
                 }
+                movePlayer(soldier, x_end, y_end, v);
                 SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
                 SDL_RenderClear(gRenderer);
+                if(dRange){
+                    drawRange(soldier);
+                }
                 renderSquare(x_pos, y_pos, square_size);
+                drawPlayer(soldier);
                 SDL_RenderPresent(gRenderer);
             }
             SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
