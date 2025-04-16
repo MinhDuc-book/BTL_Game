6f7e33a6ff5f0b0683cd67a92eae837d1418efc2
Minh Đức <mduc7325@gmail.com>
2025-04-16 17:48:57 +0700

    src file for small project

--- src/include/SDL2/SDL_revision.h
+++ src/include/SDL2/SDL_revision.h
@@ -1,8 +1,2 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.30.9 (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-2.30.9"
-#endif
+#define SDL_REVISION "hg-14525:e52d96ea04fc"
+#define SDL_REVISION_NUMBER 14525
